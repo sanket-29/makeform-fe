2523dce2f97da1eac00a0b938ca8616d2155047e
Sanket Avkirkar <avkirkarsanket29@gmail.com>
2026-06-02 22:58:40 +0530

    auth login api endpoint fixed

--- src/app/(auth)/admin-login/page.tsx
+++ src/app/(auth)/admin-login/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { Eye, EyeOff, Lock } from "lucide-react";
 import { isAuthenticated, getUserType } from "@/src/utils/auth";
+import { BASE_URL } from "@/config/api";
 
 export default function AdminLoginPage() {
   const [form, setForm] = useState({
@@ -58,7 +59,7 @@ export default function AdminLoginPage() {
       setError("");
       setMessage("");
 
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch(`${BASE_URL}/api/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -91,7 +92,7 @@ export default function AdminLoginPage() {
       setLoading(true);
       setError("");
 
-      const res = await fetch("http://localhost:5000/api/auth/verify-otp", {
+      const res = await fetch(`${BASE_URL}/api/auth/verify-otp`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
